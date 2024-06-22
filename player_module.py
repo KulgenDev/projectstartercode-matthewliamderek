5320d19a9d2b993a385b0550bcdfc56601e38549
Kulgen <matthewbk6@gmail.com>
2024-06-22 10:20:15 -0400

    added angle code

--- player_module.py
+++ player_module.py
@@ -14,13 +14,13 @@ class Player:
 
     def move(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.y -= self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.y += self.speed
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.x -= self.speed
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.x += self.speed
 
     def get_pos(self):
@@ -30,12 +30,27 @@ class Player:
     def draw(self):
         pygame.draw.rect(self.screen, pygame.Color("White"), (self.x, self.y, 20, 20))
 
+class testBullet:
+    def __init__(self, screen, x, y, angle):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.screen = screen
+
+    def move(self):
+        self.x += 10 * math.cos(self.angle)
+        self.y += 10 * math.sin(self.angle)
+
+    def draw(self):
+        pygame.draw.rect(self.screen, pygame.Color("Blue"), (self.x, self.y, 5, 5))
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     pygame.display.set_caption("Testing the Player")
     screen = pygame.display.set_mode((640, 650))
     player = Player(screen, 300, 300, 3)
+    bullets = []
 
     while True:
         clock.tick(60)
@@ -45,12 +60,19 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 angle = numpy.arctan((pos[1] - player.y)/(pos[0] - player.x))
-
-
-
-
+                if pos[0] - player.x < 0:
+                    angle += math.pi
+                bullets.append(testBullet(screen, player.x, player.y, angle))
 
         screen.fill((0, 0, 0))
+
+        for b in bullets:
+            b.move()
+            b.draw()
+
+
+
+
 
         player.move()
         player.draw()
