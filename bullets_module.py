93de67b5623cea32aa9572cb1ee494318fded3c2
Derek <DerekAJuergens@icloud.com>
2024-06-22 09:53:31 -0500

    bullet prototype

--- bullets_module.py
+++ bullets_module.py
@@ -18,10 +18,25 @@ class Bullet:
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
 
-    def move(self, angle):
+    def move(self):
         # take an angle and create a move direction
-        self.x += 10 * math.cos(angle)
-        self.y += 10 * math.sin(angle)
+        self.x += 1 * math.cos(self.angle)
+        self.y += 1 * math.sin(self.angle)
+        points = []
+        center_x = self.x + (self.width / 2)
+        center_y = self.y + (self.height / 2)
+        radius = math.sqrt((self.height / 2) ** 2 + (self.width / 2) ** 2)
+        specific_angle = math.atan2(self.height / 2, self.width / 2)
+        specific_angles = [specific_angle, -specific_angle + math.pi, specific_angle + math.pi, -specific_angle]
+        specific_radians = (math.pi / 180) * self.angle
+        for angle in specific_angles:
+            y_offset = -1 * radius * math.sin(angle + specific_radians)
+            x_offset = radius * math.cos(angle + specific_radians)
+            points.append((center_x + x_offset, center_y + y_offset))
+        pygame.draw.polygon(self.screen, self.color, points)
+
+        # credit to Tim Swast on stackoverflow.com
+
 
     def hit_enemy(self):
         # when hit by enemy, cause the enemy to be removed and remove the bullet
@@ -29,7 +44,6 @@ class Bullet:
 
     def off_screen(self):
         return (int(self.y) > int(self.screen.get_height())) or (int(self.y) < 0) or (int(self.x) > int(self.screen.get_width())) or (int(self.x) < int(0))
-        pass
 
 test_list = []
 def main():
@@ -52,7 +66,7 @@ def main():
             test_bullet.y += 10
         screen.fill(pygame.Color("Black"))
         test_bullet.draw()
-        test_bullet.move(math.radians(60))
+        test_bullet.move()
         pygame.display.update()
 
 if __name__ == "__main__":
