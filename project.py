397097d91d07ccc03a86ce303762e4654f9952c4
liamyates <yatesl4739@my.sduhsd.net>
2024-06-22 10:01:34 -0400

    initialized the pygame and made a basic window for the final project, I created the background and added colors for the map to make it look nicer.

--- project.py
+++ project.py
@@ -11,21 +11,47 @@ def main():
     pygame.init()
 
     # create a screen
-    pygame.display.set_caption("Cool Project")
+    pygame.display.set_caption("Gopher Shooter")
     # TODO: Change the size of the screen as you see fit!
-    screen = pygame.display.set_mode((640, 480))
+
+    #REMOVE THE PYGAME.FULLSCREEN AT THE END TO MAKE IT NOT FORCE FULLSCREEN, IT WILL INSTEAD BE A SQUARE 800 by 800 window.
+    screen = pygame.display.set_mode((800,800))
 
     # let's set the framerate
     clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
             if event.type == pygame.QUIT:
                 sys.exit()
 
             # TODO: Add you events code
 
         # TODO: Fill the screen with whatever background color you like!
-        screen.fill((255, 255, 255))
+        screen.fill((110, 79, 33))
+
+        ##DRAW BACKGROUND:::
+
+        #left side
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(0, 0, 120, 250))
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(0, 550, 120, 250))
+
+        #top side
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(0, 0, 250, 120))
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(500, 0, 250, 120))
+
+        #right side
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(screen.get_width()-120, 0, 120, 250))
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(screen.get_width()-120, 550, 120, 250))
+
+        #bottom side
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(0, screen.get_height()-120, 250, 120))
+        pygame.draw.rect(screen, (56, 40, 16), pygame.Rect(500, screen.get_height()-120, 250, 120))
+
+
+
 
         # TODO: Add your project code
 
