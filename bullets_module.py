857c10bb8c8026e31ad51feec1614209f97aeaae
Derek <DerekAJuergens@icloud.com>
2024-06-23 10:55:55 -0500

    did some small changes for CapsWord convention

--- bullets_module.py
+++ bullets_module.py
@@ -1,10 +1,10 @@
 import pygame
-import time
 import sys
-import player_module
+# import player_module
 import math
 
 screen = pygame.display.set_mode((700, 700))
+
 
 class Bullet:
 
@@ -20,15 +20,11 @@ class Bullet:
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
 
-
-    def spawn_sound(self):
-        bullet_sound = pygame.mixer.Sound("jasbro_laser.wav")
-        bullet_sound.play(0)
-
     def move(self):
         # take an angle and create a move direction
-        self.x += 7 * math.cos(self.angle)
-        self.y += 7 * math.sin(self.angle)
+        self.x += 1 * math.cos(self.angle)
+        self.y += 1 * math.sin(self.angle)
+
         points = []
         center_x = self.x + (self.width / 2)
         center_y = self.y + (self.height / 2)
@@ -36,14 +32,13 @@ class Bullet:
         specific_angle = math.atan2(self.height / 2, self.width / 2)
         specific_angles = [specific_angle, -specific_angle + math.pi, specific_angle + math.pi, -specific_angle]
         specific_radians = (math.pi / 180) * self.angle
-        for angle in specific_angles:
-            y_offset = -1 * radius * math.sin(angle + specific_radians)
-            x_offset = radius * math.cos(angle + specific_radians)
+        for part in specific_angles:
+            y_offset = -1 * radius * math.sin(part + specific_radians)
+            x_offset = radius * math.cos(part + specific_radians)
             points.append((center_x + x_offset, center_y + y_offset))
         pygame.draw.polygon(self.screen, self.color, points)
 
         # credit to Tim Swast on stackoverflow.com
-
 
     def hit_enemy(self):
         # when hit by enemy, cause the enemy to be removed and remove the bullet
@@ -61,23 +56,33 @@ class Bullet:
         else:
             return False
 
+
 bullet_list = []
+
 
 def bullet_spawn():
     # from player_module import angle
     # ask matthew to make angle a global variable and then change math.radians(60) to angle
-    bullet = Bullet(screen, 350, 350, 2, 4, pygame.Color("Green"), math.radians(60))
+    pos = pygame.mouse.get_pos()
+    if not (pos[0] - 350 == 0):
+        angle = math.atan((pos[1] - 350) / (pos[0] - 350))
+        if pos[0] - 350 < 0:
+            angle += math.pi
+    bullet = Bullet(screen, 350, 350, 1, 4, pygame.Color("Green"), angle)
     bullet_list.append(bullet)
     bullet_sound = pygame.mixer.Sound("jasbro_laser.wav")
+    # sound created by jobro on freesound
     bullet_sound.play(0)
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((700, 700))
     clock = pygame.time.Clock()
-
+    """
     #bullet = Bullet(screen, 1000, 1000, 2, 4, pygame.Color("Green"), math.radians(60))
     #bullet_list.append(bullet)
+    """
 
     while True:
         clock.tick(60)
@@ -87,28 +92,20 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 bullet_spawn()
                 print(bullet_list)
-        key_press = pygame.key.get_pressed()
-        if key_press[pygame.K_UP]:
-            bullet.y -= 10
-        if key_press[pygame.K_DOWN]:
-            bullet.y += 10
         screen.fill(pygame.Color("Black"))
         for bullet in bullet_list:
             bullet.draw()
             bullet.move()
             if bullet.x < 0:
                 bullet_list.remove(bullet)
-                print(bullet_list)
             if bullet.x > screen.get_width():
                 bullet_list.remove(bullet)
-                print(bullet_list)
             if bullet.y < 0:
                 bullet_list.remove(bullet)
-                print(bullet_list)
             if bullet.y > screen.get_height():
                 bullet_list.remove(bullet)
-                print(bullet_list)
         pygame.display.update()
+
 
 if __name__ == "__main__":
     main()
