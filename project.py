f261301aa6e7bf8fa584861e0df07c001027e438
liamyates <yatesl4739@my.sduhsd.net>
2024-06-24 11:44:23 -0400

    enemies slowly spawn faster and faster as time goes on

--- project.py
+++ project.py
@@ -51,6 +51,8 @@ def main():
     while True:
         final_clock = time.time()
         clock.tick(60)
+        if spawn_time - .00002 > 0:
+            spawn_time -= .00009
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
